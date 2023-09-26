ff8d1d0c46e953faf7702420bbd761b99adf3d89
ArchaQP <archa.agrawal@questionpro.com>
2023-09-26 10:50:41 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,21 @@
+//heading
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from react!!"
+);
+
+//nested structure
+const nestedElements = React.createElement("div", { id: "Parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "C1h1" }, "H1 tag!!"),
+    React.createElement("h2", { key: "C1h2" }, "H2 tag!!"),
+  ]),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "C2h1" }, "H1 tag!!"),
+    React.createElement("h2", { key: "C2h2" }, "H2 tag!!"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(nestedElements);
